95cf7a73c481b390f1bb0dc0bd71944578063402
Sarthakparmar-04 <singhsarthak691@gmail.com>
2023-12-02 14:16:24 +0530

    add a sqlite path

--- core/settings.py
+++ core/settings.py
@@ -85,20 +85,20 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-'''
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgre",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-'''
-# settings.py
 
+# settings.py
+'''
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
+'''
 
 
 # Password validation
