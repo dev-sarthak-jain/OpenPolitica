4e591fe341a038929643d3fc2735f8a11df56d42
Sarthakparmar-04 <singhsarthak691@gmail.com>
2023-12-03 00:39:32 +0530

    emove sql

--- core/settings.py
+++ core/settings.py
@@ -87,17 +87,19 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
+
+# settings.py
+'''
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
-
-# settings.py
-'''
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 '''
 
